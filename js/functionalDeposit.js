78d540cc475429175161e4a3afbc1299db24080d
emon3455 <emon.mhk69@gmail.com>
2023-02-13 00:51:04 +0600

    deposit Done

--- js/functionalDeposit.js
+++ js/functionalDeposit.js
@@ -1,5 +1,14 @@
 document.getElementById("deposit-btn").addEventListener("click", function(){
 
-    const depostAmount = getInputValueByID("deposit-input")
+    const NewdepositAmount = getInputValueByID("deposit-input");
+    const OldDepositAmount = getElementValueByID("deposit-amount");
+
+    const totallDepositAmount = NewdepositAmount + OldDepositAmount;
+
+    setElementValueByID("deposit-amount" , totallDepositAmount);
+
+    const oldBalance = getElementValueByID("balance-amount");
+    const newBalance = oldBalance + NewdepositAmount;
+    setElementValueByID("balance-amount",newBalance);
 
 });
